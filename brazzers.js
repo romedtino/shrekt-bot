9556592000c1223036552aaf9898065021fc991b
Glitch (famiree-discord) <none>
2019-04-27 04:59:05 +0000

    🌨🌔 Checkpoint ./brazzers.js:1419740/700

--- brazzers.js
+++ brazzers.js
@@ -1,5 +1,8 @@
 var filter = require('./channel_filter.js')
 var imgur = require('imgur');
+var watermark = require('watermarkjs');
+
+var imgur_ep = 'https://api.imgur.com/3/'
 
 var jsCommand = "brazzers";
 
@@ -14,21 +17,34 @@ function help_info() {
 
 function execute(command, args, message) {
   if(command === jsCommand && filter(message)) {
-    request({
-            url: cat,
-            json: true
-        }, function (error, response, body) {
-            if(body.file === undefined )
-            {
-              message.channel.send("<@" + message.author.id +"> Sorry the meow API is overloaded... try again later.");
-            }
-            else {
-              message.channel.send("<@" + message.author.id + "> meow! " + body.file);
-              message.delete()
-                .then(() => console.log("message deleted."))
-                .catch(console.error);
-            }
+    
+    watermark(['https://i.imgur.com/s5AUpuY.jpg', 'http://i.imgur.com/Ebb75ej.jpg'])
+      .image( (image1, image2) => {
+        var context = image1.getContext('2d');
+        context.save();
+        
+        context.globalAlpha = alpha;
+        context.drawImage(image2, 10, 10);
+        
+        context.restore();
+        return target;
     });
+    
+    // request({
+    //         url: cat,
+    //         json: true
+    //     }, function (error, response, body) {
+    //         if(body.file === undefined )
+    //         {
+    //           message.channel.send("<@" + message.author.id +"> Sorry the meow API is overloaded... try again later.");
+    //         }
+    //         else {
+    //           message.channel.send("<@" + message.author.id + "> meow! " + body.file);
+    //           message.delete()
+    //             .then(() => console.log("message deleted."))
+    //             .catch(console.error);
+    //         }
+    // });
   }
 
 }
