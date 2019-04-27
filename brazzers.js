b9497723f56e537f03d91a4996a025bf50465a88
Glitch (famiree-discord) <none>
2019-04-27 04:36:56 +0000

    ✂️⏳ Checkpoint ./brazzers.js:1419740/1120

---
+++ brazzers.js
@@ -0,0 +1,38 @@
+var filter = require('./channel_filter.js')
+var request = require ("request");
+var cat = "http://aws.random.cat/meow.php"
+
+var jsCommand = "brazzers";
+
+function help_info() {
+  var help = {};
+  help["command"] = jsCommand;
+  help["help"] = "Put the Brazzers logo on an image. Usage: `!brazzers [URL to image]`"
+
+  return help;
+
+}
+
+function execute(command, args, message) {
+  if(command === jsCommand && filter(message)) {
+    request({
+            url: cat,
+            json: true
+        }, function (error, response, body) {
+            if(body.file === undefined )
+            {
+              message.channel.send("<@" + message.author.id +"> Sorry the meow API is overloaded... try again later.");
+            }
+            else {
+              message.channel.send("<@" + message.author.id + "> meow! " + body.file);
+              message.delete()
+                .then(() => console.log("message deleted."))
+                .catch(console.error);
+            }
+            });
+  }
+
+}
+
+module.exports.execute = execute;
+module.exports.help_info = help_info;
