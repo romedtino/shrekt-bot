83e171c67e7f0b44fd030e2ed0e5edae5b4d5851
Glitch (famiree-discord) <none>
2019-04-27 07:42:25 +0000

    🔔🚆 Checkpoint ./brazzers.js:1419740/5926

--- brazzers.js
+++ brazzers.js
@@ -18,33 +18,41 @@ function help_info() {
 
 function execute(command, args, message) {
   if(command === jsCommand && filter(message)) {
+    var brazzersLogoURL = 'https://i.imgur.com/gSnHoXE.jpg';
     var tmpFilename = args[0];
-    if(tmpFilename === "") {
-      var seconds = new Date() / 1000;
-      tmpFilename = String(seconds);
+    if(args[0] === "") {
+      tmpFilename = 'https://i.imgur.com/s5AUpuY.jpg';
     }
-      
-      request('https://i.imgur.com/s5AUpuY.jpg', { encoding: 'binary'}, (err, res, body) => {
-        fs.writeFile('img1.jpg', body, 'binary', err => {
-            request('https://i.imgur.com/gSnHoXE.jpg', { encoding: 'binary'}, (err2, res2, body2) => {
-              fs.writeFile('img2.jpg', body2, 'binary', err => {
-                sharp('img1.jpg')
-                  .resize( { height: 1080 })
-                  .toFile('img1.png')
-                  .then( data => {
-                    sharp('img2.jpg')
-                      .resize( {
+      console.log(tmpFilename);
+      fs.unlinkSync('/tmp/img1.jpg');
+      fs.unlinkSync('/tmp/img1.jpg');
+      request(tmpFilename, { encoding: 'binary'}, (err, res, body) => {
+        fs.writeFile('/tmp/img1.jpg', body, 'binary', err => {
+            request(brazzersLogoURL, { encoding: 'binary'}, (err2, res2, body2) => {
+              fs.writeFile('/tmp/img2.jpg', body2, 'binary', err => {
+                sharp('/tmp/img1.jpg')
+                  .resize( { width: 1920 })
+                  .toFile('/tmp/img1r.png')
+                  .then( () => {
+                    sharp('/tmp/img2.jpg')
+                      .resize( { width: 480 })
+                      .toFile('/tmp/img2r.png')
+                      .then( () => {
+                          sharp('/tmp/img1r.png')
+                            .composite([{input: '/tmp/img2r.png', gravity: sharp.gravity.southeast }])
+                            .toFile('/tmp/brazzers.png')
+                            .then( data => {
+                              message.channel.send(`<@${message.author.id}> here ya go`, {
+                                  files: ['/tmp/brazzers.png']
+                                });
+                            })
+                            .catch( err => console.log(err));
+                      })
+                      .catch( err => console.log(err));
                   })
                   .catch( err => console.log(err));
                 
-                  .composite([{input: 'img2.jpg', gravity: sharp.gravity.southeast }])
-                  .toFile('/tmp/brazzers.png')
-                  .then( data => {
-                    message.channel.send(`<@${message.author.id}> here ya go`, {
-                        files: ['/tmp/brazzers.png']
-                      });
-                  })
-                  .catch( err => console.log(err));
+                  
               });
             
           });
@@ -52,47 +60,6 @@ function execute(command, args, message) {
 
       });
     
-      // sharp('https://i.imgur.com/s5AUpuY.jpg')
-      //   .composite([{input: 'https://i.imgur.com/gSnHoXE.jpg', gravity: sharp.gravity.southeast }])
-      //   .toFile('/tmp/brazzers.png')
-      //   .then( data => {
-      //     message.channel.send(`<@${message.author.id}> here ya go`, {
-      //         files: ['/tmp/brazzers.png']
-      //       });
-      //   })
-      //   .catch( err => console.log(err));
-    
-      // mergeImages(['https://i.imgur.com/s5AUpuY.jpg', 'https://i.imgur.com/gSnHoXE.jpg'], {
-      //   Canvas: Canvas
-      // })
-      //   .then( b64 => {
-      //     fs.writeFile("/tmp/brazzers.png", b64, 'base64', err => {
-      //       if(err) {
-      //         console.log(err)
-      //       } else {
-      //         message.channel.send(`<@${message.author.id}>`, {
-      //           files: ['https://i.imgur.com/s5AUpuY.jpg']
-      //         });
-      //       }
-      //     });
-      //    });
-          
-    
-    // request({
-    //         url: cat,
-    //         json: true
-    //     }, function (error, response, body) {
-    //         if(body.file === undefined )
-    //         {
-    //           message.channel.send("<@" + message.author.id +"> Sorry the meow API is overloaded... try again later.");
-    //         }
-    //         else {
-    //           message.channel.send("<@" + message.author.id + "> meow! " + body.file);
-    //           message.delete()
-    //             .then(() => console.log("message deleted."))
-    //             .catch(console.error);
-    //         }
-    // });
   }
 
 }
