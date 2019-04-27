d9f9b07bbc1e82d4ba8e0cdcff9ba93dd3130262
Glitch (famiree-discord) <none>
2019-04-27 08:15:31 +0000

    🎭👑 Checkpoint ./brazzers.js:1419740/685

--- brazzers.js
+++ brazzers.js
@@ -20,37 +20,34 @@ function execute(command, args, message) {
   if(command === jsCommand && filter(message)) {
     var brazzersLogoURL = 'https://i.imgur.com/gSnHoXE.jpg';
     var tmpFilename = args[0];
-    if(args[0] === "") {
+    if(!tmpFilename) {
       tmpFilename = 'https://i.imgur.com/s5AUpuY.jpg';
     }
     var ext = tmpFilename.split('.').pop();
     console.log(tmpFilename);
-    // fs.unlinkSync(`/tmp/img1.${ext}`, err => console.log(err));
-    // fs.unlinkSync('/tmp/img1r.jpg', err => console.log(err));
-    // fs.unlinkSync('/tmp/img2.jpg', err => console.log(err));
-    // fs.unlinkSync('/tmp/img2r.jpg', err => console.log(err));
-    // fs.unlinkSync('/tmp/brazzers.jpg', err => console.log(err));
-
+    
+    var seconds = new Date() / 1000;
+    
     request(tmpFilename, { encoding: 'binary'}, (err, res, body) => {
-      fs.writeFile(`/tmp/img1.${ext}`, body, 'binary', errWrite => {
+      fs.writeFile(`/tmp/img1${seconds}.${ext}`, body, 'binary', errWrite => {
           if(errWrite) console.log(err);
           request(brazzersLogoURL, { encoding: 'binary'}, (err2, res2, body2) => {
-            fs.writeFile('/tmp/img2.jpg', body2, 'binary', errWrite2 => {
+            fs.writeFile(`/tmp/img2${seconds}.jpg`, body2, 'binary', errWrite2 => {
               if(errWrite2) console.log(errWrite2);
-              sharp(`/tmp/img1.${ext}`)
+              sharp(`/tmp/img1${seconds}.${ext}`)
                 .resize( { width: 1920 })
-                .toFile('/tmp/img1r.jpg')
+                .toFile(`/tmp/img1r${seconds}.jpg`)
                 .then( () => {
-                  sharp('/tmp/img2.jpg')
+                  sharp(`/tmp/img2${seconds}.jpg`)
                     .resize( { width: 480 })
-                    .toFile('/tmp/img2r.jpg')
+                    .toFile(`/tmp/img2r${seconds}.jpg`)
                     .then( () => {
-                        sharp('/tmp/img1r.jpg')
-                          .composite([{input: '/tmp/img2r.jpg', gravity: sharp.gravity.southeast }])
-                          .toFile('/tmp/brazzers.jpg')
+                        sharp(`/tmp/img1r${seconds}.jpg`)
+                          .composite([{input: `/tmp/img2r${seconds}.jpg`, gravity: sharp.gravity.southeast }])
+                          .toFile(`/tmp/brazzers${seconds}.jpg`)
                           .then( data => {
                             message.channel.send(`<@${message.author.id}> here ya go`, {
-                                files: ['/tmp/brazzers.jpg']
+                                files: [`/tmp/brazzers${seconds}.jpg`]
                               });
                           })
                           .catch( err => console.log(err));
