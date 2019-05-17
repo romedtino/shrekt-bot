d6c08ba13927d6785fef9ef823948aab93392357
Glitch (famiree-discord) <none>
2019-05-17 05:14:17 +0000

    🐓🏔 Checkpoint ./brazzers.js:1419740/2038

--- brazzers.js
+++ brazzers.js
@@ -2,7 +2,6 @@ var filter = require('./channel_filter.js')
 var imgur = require('imgur');
 var fs = require('fs');
 var mergeImages = require('merge-images');
-// var sharp = require('sharp');
 const Canvas = require('canvas');
 var request = require('request');
 var jimp = require('jimp');
@@ -30,25 +29,22 @@ function brazzit(message, args) {
   var brazzersLogoURL = 'https://i.imgur.com/gSnHoXE.jpg';
   jimp.read(brazzersLogoURL)
     .then( image => {
-      console.log("read brazz");
-      image.scale(0.08)
+      
       jimp.read(tmpFilename)
         .then( targetImg => {
-          console.log("read tmp");
-          targetImg.blit(image, targetImg.bitmap.width - image.bitmap.width, 
-                         targetImg.bitmap.height - image.bitmap.width);
-          console.log("writing");
+          image.resize(targetImg.bitmap.width * 0.3, jimp.AUTO);
+          targetImg.blit(image, targetImg.bitmap.width - image.bitmap.width - 20, 
+                         targetImg.bitmap.height - image.bitmap.height - 20);
           targetImg.write(outfile, () => {
-                console.log("sending to chan");
                 message.channel.send(`<@${message.author.id}> here ya go`, {
                           files: [`${outfile}`]
                         });
             });
         })
-        .catch( e => console.log("JEROME" + e) );
+        .catch( e => console.log(e) );
   
     })
-    .catch( e => console.log("JEROME2" + e) );
+    .catch( e => console.log(e) );
   
 
 }
@@ -65,46 +61,8 @@ function execute(command, args, message) {
     
     var seconds = new Date() / 1000;
     
-    brazzit(message, args);
-    
-//     request(tmpFilename, { encoding: 'binary'}, (err, res, body) => {
-//       fs.writeFile(`/tmp/img1${seconds}.${ext}`, body, 'binary', errWrite => {
-//           if(errWrite) console.log(err);
-//           request(brazzersLogoURL, { encoding: 'binary'}, (err2, res2, body2) => {
-//             fs.writeFile(`/tmp/img2${seconds}.jpg`, body2, 'binary', errWrite2 => {
-//               if(errWrite2) console.log(errWrite2);
-//               sharp(`/tmp/img1${seconds}.${ext}`)
-//                 .resize( { width: 1920 })
-//                 .toFile(`/tmp/img1r${seconds}.jpg`)
-//                 .then( () => {
-//                   sharp(`/tmp/img2${seconds}.jpg`)
-//                     .resize( { width: 480 })
-//                     .toFile(`/tmp/img2r${seconds}.jpg`)
-//                     .then( () => {
-//                         sharp(`/tmp/img1r${seconds}.jpg`)
-//                           .composite([{input: `/tmp/img2r${seconds}.jpg`, gravity: sharp.gravity.southeast }])
-//                           .toFile(`/tmp/brazzers${seconds}.jpg`)
-//                           .then( data => {
-//                             message.channel.send(`<@${message.author.id}> here ya go`, {
-//                                 files: [`/tmp/brazzers${seconds}.jpg`]
-//                               });
-//                           })
-//                           .catch( err => console.log(err));
-//                     })
-//                     .catch( err => console.log(err));
-//                 })
-//                 .catch( err => console.log(err));
-
-
-//             });
-
-//         });
-//       });
-
-//     });
-    
+    brazzit(message, args);    
   }
-
 }
 
 module.exports.execute = execute;
