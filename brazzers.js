eb82ad57b66c615e7e1c75d1e2bc668a4b7d8c31
Glitch (famiree-discord) <none>
2019-05-17 04:08:17 +0000

    🍐👦 Checkpoint ./brazzers.js:1419740/710

--- brazzers.js
+++ brazzers.js
@@ -5,6 +5,8 @@ var mergeImages = require('merge-images');
 var sharp = require('sharp');
 const Canvas = require('canvas');
 var request = require('request');
+var jimp = require('jimp');
+
 var jsCommand = "brazzers";
 
 function help_info() {
@@ -14,6 +16,18 @@ function help_info() {
 
   return help;
 
+}
+
+async function brazzit(args) {
+  var brazzersLogoURL = 'https://i.imgur.com/gSnHoXE.jpg';
+  const brazzLogo = await jimp.read(brazzersLogoURL);
+  
+  var tmpFilename = args[0];
+  if(!tmpFilename) {
+    tmpFilename = 'https://i.imgur.com/s5AUpuY.jpg';
+  }
+  
+  const targetImg = await jimp.read(tmpFilename);
 }
 
 function execute(command, args, message) {
@@ -28,41 +42,43 @@ function execute(command, args, message) {
     
     var seconds = new Date() / 1000;
     
-    request(tmpFilename, { encoding: 'binary'}, (err, res, body) => {
-      fs.writeFile(`/tmp/img1${seconds}.${ext}`, body, 'binary', errWrite => {
-          if(errWrite) console.log(err);
-          request(brazzersLogoURL, { encoding: 'binary'}, (err2, res2, body2) => {
-            fs.writeFile(`/tmp/img2${seconds}.jpg`, body2, 'binary', errWrite2 => {
-              if(errWrite2) console.log(errWrite2);
-              sharp(`/tmp/img1${seconds}.${ext}`)
-                .resize( { width: 1920 })
-                .toFile(`/tmp/img1r${seconds}.jpg`)
-                .then( () => {
-                  sharp(`/tmp/img2${seconds}.jpg`)
-                    .resize( { width: 480 })
-                    .toFile(`/tmp/img2r${seconds}.jpg`)
-                    .then( () => {
-                        sharp(`/tmp/img1r${seconds}.jpg`)
-                          .composite([{input: `/tmp/img2r${seconds}.jpg`, gravity: sharp.gravity.southeast }])
-                          .toFile(`/tmp/brazzers${seconds}.jpg`)
-                          .then( data => {
-                            message.channel.send(`<@${message.author.id}> here ya go`, {
-                                files: [`/tmp/brazzers${seconds}.jpg`]
-                              });
-                          })
-                          .catch( err => console.log(err));
-                    })
-                    .catch( err => console.log(err));
-                })
-                .catch( err => console.log(err));
+
+    
+//     request(tmpFilename, { encoding: 'binary'}, (err, res, body) => {
+//       fs.writeFile(`/tmp/img1${seconds}.${ext}`, body, 'binary', errWrite => {
+//           if(errWrite) console.log(err);
+//           request(brazzersLogoURL, { encoding: 'binary'}, (err2, res2, body2) => {
+//             fs.writeFile(`/tmp/img2${seconds}.jpg`, body2, 'binary', errWrite2 => {
+//               if(errWrite2) console.log(errWrite2);
+//               sharp(`/tmp/img1${seconds}.${ext}`)
+//                 .resize( { width: 1920 })
+//                 .toFile(`/tmp/img1r${seconds}.jpg`)
+//                 .then( () => {
+//                   sharp(`/tmp/img2${seconds}.jpg`)
+//                     .resize( { width: 480 })
+//                     .toFile(`/tmp/img2r${seconds}.jpg`)
+//                     .then( () => {
+//                         sharp(`/tmp/img1r${seconds}.jpg`)
+//                           .composite([{input: `/tmp/img2r${seconds}.jpg`, gravity: sharp.gravity.southeast }])
+//                           .toFile(`/tmp/brazzers${seconds}.jpg`)
+//                           .then( data => {
+//                             message.channel.send(`<@${message.author.id}> here ya go`, {
+//                                 files: [`/tmp/brazzers${seconds}.jpg`]
+//                               });
+//                           })
+//                           .catch( err => console.log(err));
+//                     })
+//                     .catch( err => console.log(err));
+//                 })
+//                 .catch( err => console.log(err));
 
 
-            });
+//             });
 
-        });
-      });
+//         });
+//       });
 
-    });
+//     });
     
   }
 
